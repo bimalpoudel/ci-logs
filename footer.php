852762fed21a8c33c7ed6679e1eb2742d18888eb
Bimal Poudel <gist@bimal.org.np>
2016-06-27 17:09:09 -0600

    ob_get_flush()

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php
 # Include this file in head of CI/index.php
 
-$contents = ob_get_clean();
+$contents = ob_get_flush();
 file_put_contents(sprintf('%s/logs/urls-%s.log', dirname(__FILE__), $timestamp), $contents, FILE_APPEND);
